69254a30ce06249d3ab420f98e442931d5a62b9d
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-11-01 21:47:48 +0900

    feat: モバイルの場合にcanvasタグのwidth、heightを変える機能を追加

--- src/hooks/useDrawInitialRoullet.ts
+++ src/hooks/useDrawInitialRoullet.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
 type Props = {
   radius: number;
@@ -12,23 +12,26 @@ export const useDrawInitialRoullet = (canvas: HTMLCanvasElement | null) => {
     ctx.translate(canvas.width / 2, canvas.height / 2);
   }, []);
 
-  const drawInitialRoullet = (props: Props) => {
-    const { radius } = props;
-    if (!ctx) return;
-    ctx.clearRect(
-      -canvas.width / 2,
-      -canvas.height / 2,
-      canvas.width,
-      canvas.height
-    );
-    ctx.beginPath();
-    ctx.arc(0, 0, radius, (0 * Math.PI) / 180, (360 * Math.PI) / 180);
-    ctx.stroke();
-    ctx.font = "15px serif";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "#4A5568";
-    ctx.fillText("アイテムを登録してください", 0, 0);
-  };
+  const drawInitialRoullet = useCallback(
+    (props: Props) => {
+      const { radius } = props;
+      if (!ctx) return;
+      ctx.clearRect(
+        -canvas.width / 2,
+        -canvas.height / 2,
+        canvas.width,
+        canvas.height
+      );
+      ctx.beginPath();
+      ctx.arc(0, 0, radius, (0 * Math.PI) / 180, (360 * Math.PI) / 180);
+      ctx.stroke();
+      ctx.font = "15px serif";
+      ctx.textAlign = "center";
+      ctx.fillStyle = "#4A5568";
+      ctx.fillText("アイテムを登録してください", 0, 0);
+    },
+    [canvas]
+  );
 
   return { drawInitialRoullet };
 };
