7c0b24163e020bba3204cd4ecca5e44df0eab1f2
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-11-02 22:21:19 +0900

    feat: 編集ボタンでアイテムがinputタグに変わる機能を追加

--- src/components/molucules/RoulletItem.tsx
+++ src/components/molucules/RoulletItem.tsx
@@ -1,4 +1,4 @@
-import { FC, memo } from "react";
+import { FC, memo, useState } from "react";
 import { faCircle, faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -6,19 +6,34 @@ import { Item } from "../../types/item";
 
 export const RoulletItem: FC<Item> = memo((props) => {
   const { text, color } = props;
+  const [editFlg, setEditFlg] = useState(false);
+  const [editedText, setEditedText] = useState("");
+
+  const onClickEdit = () => {
+    setEditedText(text);
+    setEditFlg(true);
+  };
 
   return (
     <div className="flex mb-2 items-center justify-between">
-      <div className="flex">
+      <div className="flex w-full">
         <FontAwesomeIcon
           icon={faCircle}
           className="text-2xl"
           style={{ color: `${color}` }}
         />
-        <p className="ml-2">{text}</p>
+        {editFlg ? (
+          <input
+            type="text"
+            value={editedText}
+            className="border outline-none ml-2 w-[90%]"
+          />
+        ) : (
+          <p className="ml-2">{text}</p>
+        )}
       </div>
-      <div>
-        <button className="text-gray-400 text-xl">
+      <div className="flex">
+        <button className="text-gray-400 text-xl" onClick={onClickEdit}>
           <FontAwesomeIcon icon={faPen} />
         </button>
         <button className="text-red-400 text-xl ml-4">
