d944348ca5904431e5eee736c9e167f7aa479b78
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-11-03 14:38:45 +0900

    feat: 編集ボタンの切り替え機能を追加

--- src/components/molucules/RoulletItem.tsx
+++ src/components/molucules/RoulletItem.tsx
@@ -1,5 +1,6 @@
-import { FC, memo, useState } from "react";
+import { ChangeEvent, FC, memo, useCallback, useState } from "react";
 import { faCircle, faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { Item } from "../../types/item";
@@ -12,6 +13,14 @@ export const RoulletItem: FC<Item> = memo((props) => {
   const onClickEdit = () => {
     setEditedText(text);
     setEditFlg(true);
+  };
+
+  const onChangeText = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setEditedText(e.target.value);
+  }, []);
+
+  const onClickSubmit = () => {
+    setEditFlg(false);
   };
 
   return (
@@ -27,15 +36,23 @@ export const RoulletItem: FC<Item> = memo((props) => {
             type="text"
             value={editedText}
             className="border outline-none ml-2 w-[90%]"
+            onChange={onChangeText}
           />
         ) : (
           <p className="ml-2">{text}</p>
         )}
       </div>
       <div className="flex">
-        <button className="text-gray-400 text-xl" onClick={onClickEdit}>
-          <FontAwesomeIcon icon={faPen} />
-        </button>
+        {editFlg ? (
+          <button className="text-green-400 text-xl" onClick={onClickSubmit}>
+            <FontAwesomeIcon icon={faCircleCheck} />
+          </button>
+        ) : (
+          <button className="text-gray-400 text-xl" onClick={onClickEdit}>
+            <FontAwesomeIcon icon={faPen} />
+          </button>
+        )}
+
         <button className="text-red-400 text-xl ml-4">
           <FontAwesomeIcon icon={faTrashCan} />
         </button>
