a1350ef9f50a4edcf3a1ef608b1e1451c0b14af3
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-11-04 18:39:52 +0900

    refactor: useDrawCanvasの変数名を一部変更

--- src/hooks/useDrawCanvas.ts
+++ src/hooks/useDrawCanvas.ts
@@ -24,7 +24,7 @@ export const useDrawCanvas = (canvas: HTMLCanvasElement | null) => {
       if (items.length < 1) {
         drawInitialRoullet({ radius: radius });
       } else {
-        let degPart = 360 / items.length;
+        let anglePart = 360 / items.length;
         let angle = 0;
         let sumAngle = 0;
 
@@ -32,13 +32,13 @@ export const useDrawCanvas = (canvas: HTMLCanvasElement | null) => {
         items.map((data) => {
           angle = sumAngle + angleCounter + 90;
           let startAngle = ((360 - angle) * Math.PI) / 180;
-          let endAngle = ((360 - (angle + degPart)) * Math.PI) / 180;
+          let endAngle = ((360 - (angle + anglePart)) * Math.PI) / 180;
           ctx.beginPath();
           ctx.moveTo(0, 0);
           if (data.color) ctx.fillStyle = data.color;
           ctx.arc(0, 0, radius, startAngle, endAngle, true);
           ctx.fill();
-          sumAngle += degPart;
+          sumAngle += anglePart;
         });
       }
     },
