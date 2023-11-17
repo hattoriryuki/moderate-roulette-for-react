61ccb824eb625eecaef79f45e7a3caf402bc6070
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-11-17 21:33:50 +0900

    refactor: ハンバーガーメニューのコードをmap関数に修正

--- src/components/organisms/HamburgerMenu.tsx
+++ src/components/organisms/HamburgerMenu.tsx
@@ -6,43 +6,60 @@ type Props = {
 
 export const HamburgerMenu: FC<Props> = memo((props) => {
   const { isOpen } = props;
+  const onClickTerms = () => {
+    console.log("Terms");
+  };
+
+  const labelData = [
+    {
+      title: "Rules",
+      label1: "利用規約",
+      link1: onClickTerms,
+      label2: "プライバシーポリシー",
+      link2: onClickTerms,
+    },
+    {
+      title: "Creator",
+      label1: "X-Twitter",
+      link1: onClickTerms,
+      label2: "GitHub",
+      link2: onClickTerms,
+    },
+    {
+      title: "About",
+      label1: "使い方",
+      link1: onClickTerms,
+    },
+  ];
 
   return (
     <>
-      {isOpen && (
-        <div className="absolute right-5 bg-white divide-y border w-56 rounded-md py-2 shadow-md">
-          <div className="flex flex-col">
-            <p className="text-sm text-[#0A2463] my-2 mx-4">Rules</p>
-            <div className="flex flex-col text-[#2D3748]">
-              <a href="" className="py-1.5 px-3 hover:bg-gray-200">
-                利用規約
-              </a>
-              <a href="" className="py-1.5 px-3 hover:bg-gray-200">
-                プライバシーポリシー
-              </a>
+      <div
+        className={`absolute right-5 bg-white divide-y border w-56 rounded-md py-2 shadow-md ${
+          isOpen || "hidden"
+        }`}
+      >
+        {labelData.map((data) => {
+          return (
+            <div key={data.label1}>
+              <p className="text-sm text-[#0A2463] my-2 mx-4">{data.title}</p>
+              <div className="flex flex-col text-[#2D3748]">
+                <button
+                  className="py-1.5 px-3 hover:bg-gray-200 text-left"
+                  onClick={onClickTerms}
+                >
+                  {data.label1}
+                </button>
+                {data.label2 && (
+                  <button className="py-1.5 px-3 hover:bg-gray-200 text-left">
+                    {data.label2}
+                  </button>
+                )}
+              </div>
             </div>
-          </div>
-          <div>
-            <p className="text-sm text-[#0A2463] my-2 mx-4">Creactor</p>
-            <div className="flex flex-col text-[#2D3748]">
-              <a href="" className="py-1.5 px-3 hover:bg-gray-200">
-                X-Twitter
-              </a>
-              <a href="" className="py-1.5 px-3 hover:bg-gray-200">
-                GitHub
-              </a>
-            </div>
-          </div>
-          <div>
-            <p className="text-sm text-[#0A2463] my-2 mx-4">Share</p>
-            <div className="flex flex-col text-[#2D3748]">
-              <a href="" className="py-1.5 px-3 hover:bg-gray-200">
-                共有する
-              </a>
-            </div>
-          </div>
-        </div>
-      )}
+          );
+        })}
+      </div>
     </>
   );
 });
