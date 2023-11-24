9a627c5cd548bb8ed8917549053519de438df2ca
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-11-24 17:47:06 +0900

    feat: ハンバーガーメニューにCreatorリンクを追加

--- src/components/organisms/HamburgerMenu.tsx
+++ src/components/organisms/HamburgerMenu.tsx
@@ -10,6 +10,8 @@ import {
 import { SecondaryModal } from "./SecondaryModal";
 import { TermsContent } from "../molucules/TermsContent";
 import { PrivacyContent } from "../molucules/PrivacyContent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 
 type Props = {
   isOpen: boolean;
@@ -48,28 +50,6 @@ export const HamburgerMenu: FC<Props> = memo((props) => {
     return { isOpen, func };
   };
 
-  const labelData = [
-    {
-      title: "Rules",
-      label1: "利用規約",
-      link1: onClickOpenModal,
-      label2: "プライバシーポリシー",
-      link2: onClickOpenModal,
-    },
-    {
-      title: "Creator",
-      label1: "X-Twitter",
-      link1: onCloseModal,
-      label2: "GitHub",
-      link2: onCloseModal,
-    },
-    {
-      title: "About",
-      label1: "使い方",
-      link1: onCloseModal,
-    },
-  ];
-
   return (
     <>
       <SecondaryModal
@@ -99,35 +79,58 @@ export const HamburgerMenu: FC<Props> = memo((props) => {
           isOpen || "hidden"
         }`}
       >
-        {labelData.map((data) => {
-          return (
-            <div key={data.label1}>
-              <p className="text-sm text-[#0A2463] my-2 mx-4">{data.title}</p>
-              <div className="flex flex-col text-[#2D3748]">
-                <button
-                  className="py-1.5 px-3 hover:bg-gray-200 text-left"
-                  onClick={() => {
-                    let flag = 1;
-                    data.link1(flag);
-                  }}
-                >
-                  {data.label1}
-                </button>
-                {data.label2 && (
-                  <button
-                    className="py-1.5 px-3 hover:bg-gray-200 text-left"
-                    onClick={() => {
-                      let flag = 2;
-                      data.link2(flag);
-                    }}
-                  >
-                    {data.label2}
-                  </button>
-                )}
-              </div>
-            </div>
-          );
-        })}
+        <div>
+          <p className="text-sm text-[#0A2463] my-2 mx-4">Rules</p>
+          <div className="flex flex-col text-[#2D3748]">
+            <button
+              className="py-1.5 px-3 hover:bg-gray-200 text-left"
+              onClick={() => {
+                let flag = 1;
+                onClickOpenModal(flag);
+              }}
+            >
+              利用規約
+            </button>
+            <button
+              className="py-1.5 px-3 hover:bg-gray-200 text-left"
+              onClick={() => {
+                let flag = 2;
+                onClickOpenModal(flag);
+              }}
+            >
+              プライバシーポリシー
+            </button>
+          </div>
+        </div>
+        <div>
+          <p className="text-sm text-[#0A2463] my-2 mx-4">Creator</p>
+          <div className="flex flex-col text-[#2D3748]">
+            <a
+              className="flex items-center py-1.5 px-3 hover:bg-gray-200 text-left"
+              href="http://twitter.com/ryuki_runteq_27"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={faXTwitter} />
+              <p className="ml-2">X</p>
+            </a>
+            <a
+              className="flex items-center py-1.5 px-3 hover:bg-gray-200 text-left"
+              href="https://github.com/hattoriryuki"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+              <p className="ml-2">GitHub</p>
+            </a>
+          </div>
+        </div>
+        <div>
+          <p className="text-sm text-[#0A2463] my-2 mx-4">About</p>
+          <div className="flex flex-col text-[#2D3748]">
+            <button className="py-1.5 px-3 hover:bg-gray-200 text-left">
+              使い方
+            </button>
+          </div>
+        </div>
       </nav>
     </>
   );
