a45287f4cababe2f1119313f8c0455bde0dbacb0
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-11-25 19:07:07 +0900

    feat: ハンバーガーメニューにお問合せリンクを追加

--- src/components/atoms/ContactLink.tsx
+++ src/components/atoms/ContactLink.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode, memo } from "react";
 
 type Props = {
   children: ReactNode;
+  className?: string;
 };
 
 export const ContactLink: FC<Props> = memo((props) => {
@@ -12,6 +13,7 @@ export const ContactLink: FC<Props> = memo((props) => {
       href="https://twitter.com/messages/compose?recipient_id=1457235129190223872"
       target="_blank"
       className="text-teal-500 hover:underline"
+      {...props}
     >
       {children}
     </a>
