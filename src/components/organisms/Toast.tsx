457ec0e18ddf808aca1c9a90f3a2050f6dde297c
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-12-01 23:03:46 +0900

    improve: Toast.tsxをコンポーネント化

--- src/components/organisms/Toast.tsx
+++ src/components/organisms/Toast.tsx
@@ -1,5 +1,6 @@
-import { FC, memo } from "react";
+import { FC, memo, useEffect, useState } from "react";
 import {
+  IconDefinition,
   faCircleCheck,
   faCircleExclamation,
   faCircleInfo,
@@ -7,42 +8,60 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export const Toast: FC = memo(() => {
+type Props = {
+  status: "submit" | "error" | "info";
+  title: string;
+};
+
+type Status = {
+  color: string;
+  icon: IconDefinition;
+};
+
+export const Toast: FC<Props> = memo((props) => {
+  const { status, title } = props;
+  const [toastStatus, setToastStatus] = useState<Status>({
+    color: "",
+    icon: faCircleCheck,
+  });
+
+  useEffect(() => {
+    getToastData();
+  }, [status]);
+
+  const getToastData = () => {
+    let color = "";
+    let icon = faCircleCheck;
+
+    switch (status) {
+      case "submit":
+        color = "green";
+        icon = faCircleCheck;
+        break;
+      case "error":
+        color = "red";
+        icon = faCircleExclamation;
+        break;
+      case "info":
+        color = "blue";
+        icon = faCircleInfo;
+        break;
+    }
+    const newStatus = { color, icon };
+    setToastStatus(newStatus);
+  };
+
   return (
-    <>
-      <div className="flex items-center w-[300px] h-12 py-4 px-2 mb-4 rounded-lg shadow bg-green-600">
-        <div className="flex items-center justify-center w-8 h-8 text-white rounded-lg text-lg">
-          <FontAwesomeIcon icon={faCircleCheck} />
-        </div>
-        <div className="ms-3 text-mg font-normal text-white">
-          Item moved successfully.
-        </div>
-        <button className="ms-auto -mx-1.5 -my-1.5 text-white hover:text-gray-600 rounded-lg p-1.5 hover:bg-green-500 flex items-center justify-center h-8 w-8">
-          <FontAwesomeIcon icon={faXmark} />
-        </button>
+    <div
+      className={`flex items-center w-[300px] h-12 py-4 px-2 mb-4 rounded-lg shadow bg-${toastStatus.color}-600`}
+    >
+      <div className="flex items-center justify-center w-8 h-8 text-white rounded-lg text-lg">
+        <FontAwesomeIcon icon={toastStatus.icon} />
       </div>
-      <div className="flex items-center w-[300px] h-12 py-4 px-2 mb-4 rounded-lg shadow bg-red-600">
-        <div className="flex items-center justify-center w-8 h-8 text-white rounded-lg text-lg">
-          <FontAwesomeIcon icon={faCircleExclamation} />
-        </div>
-        <div className="ms-3 text-sm font-normal text-white">
-          Item has been deleted.
-        </div>
-        <button className="ms-auto -mx-1.5 -my-1.5 text-white hover:text-gray-600 rounded-lg p-1.5 hover:bg-red-500 flex items-center justify-center h-8 w-8">
-          <FontAwesomeIcon icon={faXmark} />
-        </button>
-      </div>
-      <div className="flex items-center w-[300px] h-12 py-4 px-2 mb-4 rounded-lg shadow bg-blue-600">
-        <div className="flex items-center justify-center w-8 h-8 text-white rounded-lg text-lg">
-          <FontAwesomeIcon icon={faCircleInfo} />
-        </div>
-        <div className="ms-3 text-sm font-normal text-white">
-          Improve password difficulty.
-        </div>
-        <button className="ms-auto -mx-1.5 -my-1.5 text-white hover:text-gray-600 rounded-lg p-1.5 hover:bg-blue-500 flex items-center justify-center h-8 w-8">
-          <FontAwesomeIcon icon={faXmark} />
-        </button>
-      </div>
-    </>
+      <div className="ms-3 text-mg font-normal text-white">{title}</div>
+      <button className="ms-auto -mx-1.5 -my-1.5 text-white hover:text-gray-600 rounded-lg p-1.5 hover:bg-green-500 flex items-center justify-center h-8 w-8">
+        <FontAwesomeIcon icon={faXmark} />
+      </button>
+    </div>
   );
 });
